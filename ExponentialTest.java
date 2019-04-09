62ab99038d4fe60c2d3e27f1cd51043ce34977fc
Kyler Kopacz <kylerkopacz@vlan416-250.subnet-255.amherst.edu>
2019-04-09 12:56:17 -0400

    Removed the class files

--- ExponentialTest.java
+++
@@ -1,8 +0,0 @@
-public class ExponentialTest {
-    public static void main(String[] args) {
-        Exponential e = new Exponential(.5);
-        for(int i = 0; i < 100; i++) {
-            System.out.println(e.next());
-        }
-    }
-}

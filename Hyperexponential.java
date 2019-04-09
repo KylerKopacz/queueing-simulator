e022917307f8ebac359bbe21f32a11cc607eb1bf
Connor Haugh <connorhaugh@vlan401-170.subnet-240.amherst.edu>
2019-04-09 12:39:33 -0400

    HyperExpo fleshed out

--- Hyperexponential.java
+++ Hyperexponential.java
@@ -1,13 +1,26 @@
+import java.util.Random;
+import java.lang.Math;
 public class Hyperexponential {
-    int muOne;
-    int mewTwo; /* I had to do it */
-    int p; /* I really don't know what this is tbh */
-    
-    public Hyperexponential() {
+    double lOne;
+    double lTwo; /* the two rates of our distributions */
+    double p;
+    Random rand = new Random();
 
+
+    public Hyperexponential(double lamdaOne, double lamdaTwo, double parameter) {
+      lOne=lamdaOne;
+      lTwo=lamdaTwo;
+      p=parameter;
     }
 
-    public double next() {
-        return 0.0;
+    public int next() {
+      // generate the next instance of a Hyperexponential, using the Inverse transform method on an Exponential distibution with either lamdaOne or lamdaTwo as the parameters
+       // formula x = -(1/lamda)(ln(1- (# from uniform 1-0)))
+      if(rand.nextDouble() < p){ //go with exp distro 1
+        return (int)((-1.0)*(1.0/lOne)* Math.log(1-rand.nextDouble()));
+      }
+      else{ // go with exp distro 2
+        return (int)((-1.0)*(1.0/lTwo)* Math.log(1-rand.nextDouble()));
+      }
     }
 }

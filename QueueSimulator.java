0c83a1f1c9b8a80c29d357daa9af22d551d11bb2
Kyler Kopacz <kylerkopacz@vlan416-250.subnet-255.amherst.edu>
2019-04-10 22:25:55 -0400

    Merged the simulation into one method in the SimQueue file

--- QueueSimulator.java
+++ QueueSimulator.java
@@ -1,36 +1,7 @@
 public class QueueSimulator {
     /* This is where the actual simulator will run. I plan on adding arguments to the command line so we can run various tests without changing code. I will work on this after I finish the structure of the Queue system. */
     public static void main(String[] args) {
-        //TODO - make changes for different ns, rates
-
-        int n =100;// number of jobs
-
-        SimQueue simque = new SimQueue();
-
-        while (n>0){
-
-          System.out.println("Ariv: " + simque.nextArrivalTime  + " Dep: " + simque.nextDepartureTime + "time: " + simque.time);
-
-
-          // if the queue is empty, or the next arrival time is first
-          if(simque.nextArrivalTime < simque.nextDepartureTime || !simque.hasJob){
-            simque.time = simque.nextArrivalTime; // advance system time to next arrival
-            simque.generateJob();
-            simque.nextArrivalTime= simque.getNextArrivalTime(); // set new arrival time
-            n--;
-          }
-            // if the queue is non-empty and a departure is sooner.
-            else{
-              simque.time = simque.nextDepartureTime; //advance system time to departure time
-              simque.finishJob();
-              simque.nextDepartureTime= simque.getNextDepartureTime();// get new departure time
-              if(simque.time == simque.nextArrivalTime){
-                simque.generateJob();
-                simque.nextArrivalTime= simque.getNextArrivalTime();
-                n--;
-              }
-            }
-          }
-        System.out.println("Jobs In Service after n steps: " + simque.getJobsInService());
+        SimQueue sim = new SimQueue(0.5, 0.5, 0.5, 0.5);                          
+        sim.runSimulation(100);
     }
 }
--- SimQueue.java
+++ SimQueue.java
@@ -2,54 +2,85 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class SimQueue {
-    int time; /* time-stamp for the current state of the queue */
-    int nextArrivalTime; /* Arrival time is the time that the job enters service */
-    int nextDepartureTime; /* Departure time is the time that the job will leave the server */
-    Queue<Integer> queue;
-    Exponential exp;
-    Hyperexponential hexp;
-    boolean hasJob;
-
-
-    public SimQueue() {
-        time = 0;
-        nextArrivalTime = 0;
-        nextDepartureTime = 0;
-        queue = new LinkedList<Integer>();
-        exp = new Exponential(0.5);
-        hasJob = true;
-        hexp = new Hyperexponential(.5, .5, .5);
-        queue.add(hexp.next());
+  int time; /* time-stamp for the current state of the queue */
+  int nextArrivalTime; /* Arrival time is the time that the job enters service */
+  int nextDepartureTime; /* Departure time is the time that the job will leave the server */
+  Queue<Integer> queue;
+  Exponential exp;
+  Hyperexponential hexp;
+  boolean hasJob;
+  
+  
+  public SimQueue(double hexpLambdaOne, double hexpLambdaTwo, double hexpP, double expLambda) {
+    time = 0;
+    nextArrivalTime = 0;
+    nextDepartureTime = 0;
+    queue = new LinkedList<Integer>();
+    exp = new Exponential(expLambda);
+    hasJob = true;
+    hexp = new Hyperexponential(hexpLambdaOne, hexpLambdaTwo, hexpP);
+    queue.add(hexp.next());
+  }
+  
+  public int getNextArrivalTime() {
+    nextArrivalTime = time + exp.next();
+    return nextArrivalTime;
+  }
+  
+  public int getNextDepartureTime() {
+    return nextDepartureTime;
+  }
+  
+  public int generateJob() {
+    queue.add(hexp.next());//generate & add a job to the queue
+    
+    if(queue.size() == 1) { // job directly enters service
+      nextDepartureTime= time + queue.peek();
     }
-
-    public int getNextArrivalTime() {
-        nextArrivalTime = time + exp.next();
-        return nextArrivalTime;
+    hasJob=true;
+    return queue.peek();
+  }
+  
+  public void finishJob(){
+    //remove first element
+    queue.poll();
+    if(queue.isEmpty()) hasJob= false;
+    else{
+      nextDepartureTime = time + queue.peek();
     }
-
-    public int getNextDepartureTime() {
-            return nextDepartureTime;
-    }
-
-    public int generateJob() {
-        queue.add(hexp.next());//generate & add a job to the queue
-
-        if(queue.size() == 1) { // job directly enters service
-          nextDepartureTime= time + queue.peek();
+  }
+  
+  public int getJobsInService(){
+    return queue.size();
+  }
+  
+  public void runSimulation(int numJobs) {
+    int n = numJobs;// number of jobs
+    
+    while (n>0){
+      
+      System.out.println("Ariv: " + this.nextArrivalTime  + " Dep: " + this.nextDepartureTime + " Time: " + this.time);
+      
+      
+      // if the queue is empty, or the next arrival time is first
+      if(this.nextArrivalTime < this.nextDepartureTime || !this.hasJob){
+        this.time = this.nextArrivalTime; // advance system time to next arrival
+        this.generateJob();
+        this.nextArrivalTime= this.getNextArrivalTime(); // set new arrival time
+        n--;
+      }
+      // if the queue is non-empty and a departure is sooner.
+      else{
+        this.time = this.nextDepartureTime; //advance system time to departure time
+        this.finishJob();
+        this.nextDepartureTime= this.getNextDepartureTime();// get new departure time
+        if(this.time == this.nextArrivalTime){
+          this.generateJob();
+          this.nextArrivalTime= this.getNextArrivalTime();
+          n--;
         }
-        hasJob=true;
-        return queue.peek();
+      }
     }
-
-    public void finishJob(){
-        //remove first element
-        queue.poll();
-        if(queue.isEmpty()) hasJob= false;
-        else{
-          nextDepartureTime = time + queue.peek();
-        }
-    }
-    public int getJobsInService(){
-      return queue.size();
-    }
+    System.out.println("Jobs In Service after n steps: " + this.getJobsInService());
+  }
 }

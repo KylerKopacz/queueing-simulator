961fd6f4d76c7092d7157ee3454f33a3c30fbf97
Kyler Kopacz <kylerkopacz@vlan416-250.subnet-255.amherst.edu>
2019-04-12 00:32:08 -0400

    The final push

--- TestEnviroment.java
+++ TestEnviroment.java
@@ -20,17 +20,30 @@ public class TestEnviroment{
 
     TwoServerSimQueue sim2;
 
-    System.out.println("\n\nStarting Two Server");
+    System.out.println("\n\nStarting Two Server with coinflip");
     for (expLambda=0.1; expLambda <0.7; expLambda= expLambda + 0.1){
       //System.out.println("Lambda is : " + expLambda);
       sim2 = new TwoServerSimQueue(1,1,0.5,expLambda);
-      System.out.println("1, " + sim2.runSimulation()/expLambda + ", " + expLambda);
+      System.out.println("1, " + sim2.runSimulation(true)/expLambda + ", " + expLambda);
       sim2 = new TwoServerSimQueue(1.904,0.096,0.952,expLambda);
-      System.out.println("10, " + sim2.runSimulation()/expLambda + ", " + expLambda);
+      System.out.println("10, " + sim2.runSimulation(true)/expLambda + ", " + expLambda);
       sim2 = new TwoServerSimQueue(1.952,0.048,0.976,expLambda);
-      System.out.println("20, " + sim2.runSimulation()/expLambda + ", " + expLambda);
+      System.out.println("20, " + sim2.runSimulation(true)/expLambda + ", " + expLambda);
       sim2 = new TwoServerSimQueue(1.98,0.02,0.99,expLambda);
-      System.out.println("50, " + sim2.runSimulation()/expLambda + ", " + expLambda);
+      System.out.println("50, " + sim2.runSimulation(true)/expLambda + ", " + expLambda);
+    }
+
+    System.out.println("\n\nStarting Two Server with smaller queue size picking");
+    for (expLambda=0.1; expLambda <0.7; expLambda= expLambda + 0.1){
+      //System.out.println("Lambda is : " + expLambda);
+      sim2 = new TwoServerSimQueue(1,1,0.5,expLambda);
+      System.out.println("1, " + sim2.runSimulation(false)/expLambda + ", " + expLambda);
+      sim2 = new TwoServerSimQueue(1.904,0.096,0.952,expLambda);
+      System.out.println("10, " + sim2.runSimulation(false)/expLambda + ", " + expLambda);
+      sim2 = new TwoServerSimQueue(1.952,0.048,0.976,expLambda);
+      System.out.println("20, " + sim2.runSimulation(false)/expLambda + ", " + expLambda);
+      sim2 = new TwoServerSimQueue(1.98,0.02,0.99,expLambda);
+      System.out.println("50, " + sim2.runSimulation(false)/expLambda + ", " + expLambda);
     }
   }
 }
--- TwoServerSimQueue.java
+++ TwoServerSimQueue.java
@@ -35,17 +35,44 @@ public class TwoServerSimQueue{
   
   
   
-  public void generateJob(){
+  public void generateJob(boolean coinFlip){
     int job = hexp.next();
-    if(rand.nextBoolean()){
-      queueOne.add(job);
-      hasJobOne = true;
-      if(queueOne.size() == 1) nextDepartureTimeOne = time + queueOne.peek();
-    } else {
-      queueTwo.add(job);
-      hasJobTwo=true;
-      if(queueTwo.size() == 1) nextDepartureTimeTwo= time + queueTwo.peek();
+    if(coinFlip) {
+      if(rand.nextBoolean()){
+        queueOne.add(job);
+        hasJobOne = true;
+        if(queueOne.size() == 1) nextDepartureTimeOne = time + queueOne.peek();
+      } else {
+        queueTwo.add(job);
+        hasJobTwo=true;
+        if(queueTwo.size() == 1) nextDepartureTimeTwo= time + queueTwo.peek();
+      }
+    } else {//do it based on job size
+      if(queueOne.size() < queueTwo.size()) {
+        queueOne.add(job);
+        hasJobOne = true;
+        if(queueOne.size() == 1) nextDepartureTimeOne = time + queueOne.peek();
+      } else if(queueOne.size() > queueTwo.size()) {
+        queueTwo.add(job);
+        hasJobTwo=true;
+        if(queueTwo.size() == 1) nextDepartureTimeTwo= time + queueTwo.peek();
+      } else {//they are the same size
+        if(nextDepartureTimeOne < nextDepartureTimeTwo) {
+          queueOne.add(job);
+          hasJobOne = true;
+          if(queueOne.size() == 1) nextDepartureTimeOne = time + queueOne.peek();
+        } else if(nextDepartureTimeOne > nextDepartureTimeTwo) {
+          queueTwo.add(job);
+          hasJobTwo=true;
+          if(queueTwo.size() == 1) nextDepartureTimeTwo= time + queueTwo.peek();
+        } else {
+          queueOne.add(job);
+          hasJobOne = true;
+          if(queueOne.size() == 1) nextDepartureTimeOne = time + queueOne.peek();
+        }
+      }
     }
+    
   }
   
   public void finishJobOne(){
@@ -67,30 +94,30 @@ public class TwoServerSimQueue{
       nextDepartureTimeTwo = time + queueTwo.peek();
     } 
   }
-
+  
   public int getJobsInService(){
     return queueOne.size() + queueTwo.size();
   }
   
-  public int runSimulation(){
+  public int runSimulation(boolean coinFlip){
     ArrayList<Integer> snapshots = new ArrayList<Integer>();
     int n = 0;
     
     //kick off the simulation
-    generateJob();
+    generateJob(coinFlip);
     nextArrivalTime = time + exp.next();
     n++;
     
     while(n < 10000000){
       
       if(n > 100000)
-        snapshots.add(getJobsInService());
-
+      snapshots.add(getJobsInService());
+      
       //System.out.println(" Time: " + this.time + " Ariv: " + this.nextArrivalTime  + " Departure One: " + this.nextDepartureTimeOne  + " Departure Two: " + this.nextDepartureTimeTwo);
       
       if((nextArrivalTime < nextDepartureTimeOne || nextArrivalTime < nextDepartureTimeTwo)) {
         time = nextArrivalTime;
-        generateJob();
+        generateJob(coinFlip);
         nextArrivalTime += exp.next();
         n++;
       } else if(nextArrivalTime == nextDepartureTimeOne || nextArrivalTime == nextDepartureTimeTwo) {
@@ -98,15 +125,15 @@ public class TwoServerSimQueue{
         if(nextDepartureTimeOne == nextDepartureTimeTwo) {
           finishJobOne();
           finishJobTwo();
-          generateJob();
+          generateJob(coinFlip);
           n++;
         } else if(nextArrivalTime == nextDepartureTimeOne) {
           finishJobOne();
-          generateJob();
+          generateJob(coinFlip);
           n++;
         } else {
           finishJobTwo();
-          generateJob();
+          generateJob(coinFlip);
           n++;
         }
         nextArrivalTime += exp.next();
@@ -131,18 +158,18 @@ public class TwoServerSimQueue{
         }
       } else if(nextDepartureTimeOne == 0 && nextDepartureTimeTwo == 0) {
         time = nextArrivalTime;
-        generateJob();
+        generateJob(coinFlip);
         nextArrivalTime += exp.next();
         n++;
       }
     }
-
+    
     int sum = 0;
     for(Integer i: snapshots) {
       sum += i;
     }
     int averageJobs = (int)sum/snapshots.size();
-
+    
     //System.out.println("The average number of jobs in the queue was: " + averageJobs);
     
     return averageJobs;

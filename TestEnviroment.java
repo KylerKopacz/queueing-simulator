a29d6031f48e6cef1af8ea5ec4320225be9d4411
Kyler Kopacz <kylerkopacz@vlan416-250.subnet-255.amherst.edu>
2019-04-12 00:00:50 -0400

    Another push. What the changes are is a mystery

--- TestEnviroment.java
+++ TestEnviroment.java
@@ -6,30 +6,31 @@ public class TestEnviroment{
     SimQueue sim1;
     
     
-    for (expLambda=0.1; expLambda <0.7; expLambda= expLambda + 0.1){
-      System.out.println("Lambda is : " + expLambda);
+    for (expLambda=0.1; expLambda <0.7; expLambda = expLambda + 0.1){
+      //System.out.println("Lambda is : " + expLambda);
       sim1 = new SimQueue(1,1,0.5,expLambda);
-      System.out.println("Variance: 1 Mean Response Time: " + sim1.runSimulation()/expLambda);
+      System.out.println("1, " + sim1.runSimulation()/expLambda + ", " + expLambda);
       sim1 = new SimQueue(1.904,0.096,0.952,expLambda);
-      System.out.println("Variance: 10 Mean Response Time: " + sim1.runSimulation()/expLambda);
+      System.out.println("10, " + sim1.runSimulation()/expLambda + ", " + expLambda);
       sim1 = new SimQueue(1.952,0.048,0.976,expLambda);
-      System.out.println("Variance: 20 Mean Response Time: " + sim1.runSimulation()/expLambda);
+      System.out.println("20, " + sim1.runSimulation()/expLambda + ", " + expLambda);
       sim1 = new SimQueue(1.98,0.02,0.99,expLambda);
-      System.out.println("Variance: 50 Mean Response Time: " + sim1.runSimulation()/expLambda);
+      System.out.println("50, " + sim1.runSimulation()/expLambda + ", " + expLambda);
     }
 
     TwoServerSimQueue sim2;
 
+    System.out.println("\n\nStarting Two Server");
     for (expLambda=0.1; expLambda <0.7; expLambda= expLambda + 0.1){
-      System.out.println("Lambda is : " + expLambda);
+      //System.out.println("Lambda is : " + expLambda);
       sim2 = new TwoServerSimQueue(1,1,0.5,expLambda);
-      System.out.println("Variance: 1 Mean Response Time: " + sim2.runSimulation()/expLambda);
+      System.out.println("1, " + sim2.runSimulation()/expLambda + ", " + expLambda);
       sim2 = new TwoServerSimQueue(1.904,0.096,0.952,expLambda);
-      System.out.println("Variance: 10 Mean Response Time: " + sim2.runSimulation()/expLambda);
+      System.out.println("10, " + sim2.runSimulation()/expLambda + ", " + expLambda);
       sim2 = new TwoServerSimQueue(1.952,0.048,0.976,expLambda);
-      System.out.println("Variance: 20 Mean Response Time: " + sim2.runSimulation()/expLambda);
+      System.out.println("20, " + sim2.runSimulation()/expLambda + ", " + expLambda);
       sim2 = new TwoServerSimQueue(1.98,0.02,0.99,expLambda);
-      System.out.println("Variance: 50 Mean Response Time: " + sim2.runSimulation()/expLambda);
+      System.out.println("50, " + sim2.runSimulation()/expLambda + ", " + expLambda);
     }
   }
 }
